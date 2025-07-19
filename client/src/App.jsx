d30a0ce8ce95bbbc65da31821a1708894b3f11c0
Svaatsalya <vaatsalyasahu@gmail.com>
2025-07-19 12:45:35 +0530

    KYA And Team Page

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import KYA from "./components/KYA";
+import Team from "./components/TeamPage";
+import TeamSection from "./components/TeamPage";
+
+export default function App() {
+  return (
+    <div>
+      
+     <TeamSection />
+    </div>
+  )
+}
---
+++ client/src/lib/data.js
@@ -0,0 +1,95 @@
+
+//data about the alumni 
+
+export const AlumniData = [ 
+    {
+        id:1,
+        name: "Mr. Siddhartha Duggirala" ,
+        batch: "2010-2014",
+        photo: "../public/Alumni1.png",
+        role: "Software Engineer at Thought Farmer",
+        LinkedIn: "https://www.linkedin.com/in/siddhartha-duggirala/",
+        CompanyLink: "https://www.thoughtfarmer.com/",
+        bio: "Siddhartha Duggirala is a 2014 B.Tech alumnus from the Department of Computer Science and Engineering. With over a decade of experience building award-winning enterprise applications, he now works as a Software Engineer at ThoughtFarmer while contributing to AI-driven product development. From streamlining public-sector systems at Bharat Petroleum to co-founding startups, his journey exemplifies technical excellence, leadership, and a deep commitment to scalable, real-world innovation."
+    } ,
+    {
+         id:2,
+        name: "Mr. Aditya Sharma" ,
+        batch: "2017-2021",
+        photo: "../public/Alumni2.png",
+        role: "Assistant Executive Engineer in the CPWD",
+         LinkedIn: "https://www.linkedin.com/in/aditya-sharma-215170160/",
+         CompanyLink: "https://cpwd.gov.in/",
+        bio: "Aditya Prakash Sharma is a 2021 Btech alumnus from the department of Civil Engineering . He hails from Jaipur and is currently working in the Central Public Works Department(CPWD) as an Assistant Executive Engineer .Having always been a sports enthusiast, he was a part of the cricket team at IITI. He started off doing coding but later realized that his real passion was in Civil Engineering."
+    },
+    {
+         id:3,
+        name: "Mr. Dheeraj Bansal" ,
+        batch: "2018-2022",
+        photo: "../public/Alumni3.png",
+        role: "content writer at Thinker's Studio",
+       LinkedIn: "https://www.linkedin.com/in/dheerajbansal53/",
+       CompanyLink: "",
+        bio: "Dheeraj Bansal is a 2022 B.Tech alumnus from Department of Civil Engineering. Currently leading Thinker's Studio- A creative writing studio publishing non fiction titles . He embodied a unique fusion of creativity,psychology, and entrepreneurship.Whether it was onboarding top creators at Confluencr or poineering influencer marketing strategies for brands like Lenskart,Wrangler and Fable Street,his work speeks volume.  "
+    },
+     {
+         id:4,
+        name: "Ms. Parul Gupta" ,
+        batch: "2013-2017",
+        photo: "../public/Alumni4.png",
+        role: "Senior Production Engineer at Meta",
+        LinkedIn: "https://www.linkedin.com/in/parulgupta04/",
+        CompanyLink: "https://www.meta.com/in/",
+        bio: "Parul Gupta is a 2017 B.Tech alumnus from the Department of Computer Science and Engineering. She is currently a Senior Production Engineer at Meta, where she works within the Python Foundation team. She is also an early contributor to FairLearn, an open-source Python library to help assessand mitigate bias in AI systems. She believes in empowering open-source and actively promotes its contributions.Parul is recognized as one of the Finalist of Women in AI , North America 2025 under Responsible AI Leader for her work and advocacy on AI Ethics and Fairness . She desires to bridge the gender gap in technology and promote diversity in the tech industry.Her journey is a testament to her dedication to innovation, collaboration, and the pursuit of excellence."
+    },
+    {
+        id:5,
+        name: "Mr. Piyush Lahoti" ,
+        batch: "2010-2014",
+        photo: "../public/Alumni5.png",
+        role: "Co-Founder of Activv Money",
+        LinkedIn: "https://www.linkedin.com/in/piyushlahoti/",
+        CompanyLink: "https://activv.money/",
+        bio: "Piyush Lahoti, an accomplished alumnus of IIT Indore (Computer Science & Engineering) and IIM Ahmedabad, is currently the CoFounder of Activv Money. With over a decade of diverse experience across fintech, product management, and software development, he has led critical initiatives at Fi Money as Head of Investments &Personal Finance, and served in key roles at Media.net, Housing.com, and CA Technologies.At IIT Indore, he was the founding member and lead coordinator of the Student Entrepreneurship Support Cell (SIRC), laying its groundwork. His academic and competitive achievements include being a national winner of the Procter & Gamble CEO Challenge, top 20 in the State Science Talent Search Examination, and a finalist in ACM ICPC regionals, representing India internationally His trajectory from a curious problem-solver to a leader in India's startup ecosystem stands as a source of inspiration for the IIT Indore community."
+    },
+     {
+        id:6,
+        name: "Mr. Archit Karandikar" ,
+        batch: "2010-2014",
+        photo: "../public/Alumni6.png",
+        role: "Co-Founder of CoInventAI",
+        LinkedIn: "https://www.linkedin.com/in/architkarandikar/",
+        CompanyLink: "https://airial.travel/",
+        bio: " Archit Karandikar works on modelling of simulated agents for AI-powered validation of Self-Driving Car software at Waymo. Before this, He was at Google Research working on AI Explainability and Reliability. Prior to that, He was at Facebook and have worked on the Stories Delivery Team and the Natural Language Understanding Team. His industrial areas of expertise are artificial intelligence, product backend development at huge scale and algorithms. His academic research interests lie in Machine Learning, Theoretical Computer Science and Discrete Math. Specifically, He have published work on Reliable Deep Learning, Approximation Algorithms for Combinatorial Optimization problems and pure Combinatorics. As a competitive programmer, He have been fortunate to represent India three times - ICPC World Finals 2013, 2014 and IOI 2010 (Bronze Medal). The performances of his teams at ICPC 2014 and IOI 2010 are amongst India's best performances. He was ranked India #1 and had the Grandmaster title on the Codeforces platform when he stopped competing regularly in 2014."
+    },
+    {
+        id:7,
+        name: "Mr. Rahul Pawar" ,
+        batch: "2010-2014",
+        photo: "../public/Alumni7.png",
+        role: "Co-Founder of Banva",
+        LinkedIn: "https://www.linkedin.com/in/rahul-pawar-74a093147/",
+        CompanyLink: "https://banva.io/",
+         bio: "A 2014 B.Tech graduate in Electrical Engineering and a co-founder of Banva, he builds software products for content creation. He is currently focused on developing a subtitling product. Previously, he held the role of Senior Research Scientist at Motorola Solutions, based in the Greater Chicago area. During this time, he contributed to research and engineering in speech and audio processing, specializing in speaker identification, language identification, audio characterization, and automatic speech recognition. "
+    },
+    {
+        id:8,
+        name: "Mr. Pranav Nair" ,
+        batch: "2010-2014",
+        photo: "../public/Alumni8.png",
+        role: "Officer at BARC",
+        LinkedIn: "https://www.linkedin.com/in/rahul-pawar-74a093147/",
+        CompanyLink: "https://www.barc.gov.in/",
+         bio: "A 2014 B.Tech (EE) graduate, he is a Scientific Officer at the Bhabha Atomic Research Centre (BARC) in Mumbai, Maharashtra. Previously, he worked as an Assistant Manager at AVTEC, CK Birla Group. At BARC, he oversees the safety aspects of commercial nuclear reactors across India. He is also involved in developing safety policies and strategies for the deployment of emerging nuclear reactor designs, such as Small Modular Reactors (SMRs), in India."
+    },
+    {
+        id:9,
+        name: "" ,
+        batch: "2010-2014",
+        photo: "../public/Alumni9.png",
+        role: "Officer at BARC",
+        LinkedIn: "https://www.linkedin.com/in/rahul-pawar-74a093147/",
+        CompanyLink: "https://www.barc.gov.in/",
+         bio: ""
+    }
+]   
