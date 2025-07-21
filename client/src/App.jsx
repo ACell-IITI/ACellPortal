97d8d6b2e9caf90e83d6e455a7b5b21873e18751
Aviral Sharma <aviral0201sharma@gmail.com>
2025-07-21 11:59:02 +0530

    conflicts resolved

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,20 +2,14 @@ import { useState } from "react";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-// Common Components
 import Home from "./Home/Home";
 import Footer from "./Components/Footer/Footer";
 import Navbar from "./Components/Navbar/Navbar";
 import RegistrationForm from "./Components/RegistrationForm/RegistrationForm";
 
-// From `admin-dashboard`
-import LoginForm from "./Components/LoginPage/LoginForm";
-import CVReviewForm from "./Components/CVReviewPage/CVReviewForm";
-import KYA from "./pages/KYA";
-import Newsletter from "./pages/Newsletter";
-import Magazine from "./pages/Magazine";
 
-// From `main` (alternative paths)
+import KYA from "./pages/KYA";
+
 import LoginPage from "./pages/LoginPage";
 import CVReviewPage from "./pages/CVReviewPage";
 import SignUpPage from "./pages/SignUpPage";
@@ -32,14 +26,11 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/RegistrationForm" element={<RegistrationForm />} />
 
-          {/* Keep only one login/signup flow - adjust as needed */}
           <Route path="/Login" element={<LoginPage />} />
           <Route path="/SignUp" element={<SignUpPage />} />
 
-          {/* CV Review */}
           <Route path="/cv-review" element={<CVReviewPage />} />
 
-          {/* Pages */}
           <Route path="/Newsletter" element={<NewsletterPage />} />
           <Route path="/Magazine" element={<MagazinePage />} />
           <Route path="/KYA" element={<KYA />} />
