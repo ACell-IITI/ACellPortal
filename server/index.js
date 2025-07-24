3f4c7d832f0c805d41db5e44d31b0598367eb36f
Anish-gthub <me240003009@iiti.ac.in>
2025-07-25 02:24:27 +0530

    cv review

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ import express from "express";
 import authRoutes from "./routes/auth.js";
 import adminRoute from './routes/admin.route.js'
 import alumniRoute from './routes/alumni.route.js'
+import cv_reviewRoute from './routes/cv_review.js'
 const app = express();
 
 app.use(express.json());
@@ -14,6 +15,7 @@ app.get("/", (req, res) => {
 app.use("/auth", authRoutes);
 app.use("/admin",adminRoute);
 app.use("/alumni",alumniRoute);
+app.use("/cv",cv_reviewRoute);
 
 const PORT = process.env.PORT | 8000;
 app.listen(PORT, () => console.log(`Server started at PORT ${PORT}`));
