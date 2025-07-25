eb69d541d01a620785fa7842b12a3708d9511166
Anish-gthub <me240003009@iiti.ac.in>
2025-07-25 15:20:14 +0530

    Transferred cv_review_route to server/index.js in main branch

--- server/index.js
+++ server/index.js
@@ -3,7 +3,6 @@ import express from "express";
 import authRoutes from "./routes/auth.js";
 import adminRoute from './routes/admin.route.js'
 import alumniRoute from './routes/alumni.route.js'
-import cv_reviewRoute from './routes/cv_review.js'
 const app = express();
 
 app.use(express.json());
@@ -15,7 +14,6 @@ app.get("/", (req, res) => {
 app.use("/auth", authRoutes);
 app.use("/admin",adminRoute);
 app.use("/alumni",alumniRoute);
-app.use("/cv",cv_reviewRoute);
 
 const PORT = process.env.PORT | 8000;
 app.listen(PORT, () => console.log(`Server started at PORT ${PORT}`));
