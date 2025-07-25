dfbfb8179d9dbbcd660e1fcfa783de97bfd6d415
Anish-gthub <me240003009@iiti.ac.in>
2025-07-25 15:26:47 +0530

    added cv_review route

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,11 +2,13 @@ import authRoutes from "./auth.js";
 import adminRoute from './admin.js'
 import alumniRoute from './alumni.js'
 import mentorsRoute from './mentors.js'
+import cv_reviewRoute from './cv_review.js'
 import { Router } from "express";
 
 const router = Router();
 router.use("/auth", authRoutes);
 router.use("/admin",adminRoute);
 router.use("/alumni",alumniRoute);
-router.use('/mentors',mentorsRoute)
+router.use('/mentors',mentorsRoute);
+router.use("/cv",cv_reviewRoute);
 export default router;
