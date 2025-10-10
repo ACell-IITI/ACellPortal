53b586772f73f3197c2bd8470f25ba195e582b83
Chetan Verma <coutcv28@gmail.com>
2025-10-10 17:19:49 +0530

    Add files via upload

---
+++ server/models/Magazine_model.js
@@ -0,0 +1,9 @@
+import mongoose from 'mongoose';
+
+const magazineSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  pdfUrl: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now }
+});
+
+export default mongoose.model('Magazine', magazineSchema);
