36d8abc7c628fc7fb8f1d119f1ffa7195cef396f
Chetan Verma <coutcv28@gmail.com>
2025-10-23 23:48:43 +0530

    Add publicId field to magazine schema

--- server/models/Magazine_model.js
+++ server/models/Magazine_model.js
@@ -3,7 +3,9 @@ import mongoose from 'mongoose';
 const magazineSchema = new mongoose.Schema({
   title: { type: String, required: true },
   pdfUrl: { type: String, required: true },
+  publicId: { type: String },
   createdAt: { type: Date, default: Date.now }
 });
 
 export default mongoose.model('Magazine', magazineSchema);
+
