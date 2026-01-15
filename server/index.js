d62d94a7880a914bb27d1becff36a74b44d9a88f
Aviral Sharma <aviral0201sharma@gmail.com>
2026-01-15 20:05:44 +0530

    ok

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ const app = express();
 // Middleware
 app.use(
   cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://alumnicell.iiti.ac.in',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
     credentials: true,
   })
